ed1d4407ee75b1622d9da31d3ead7ec70e5c7993
zanenasrallah <71055457+zanenasrallah@users.noreply.github.com>
2021-01-28 19:56:02 -0600

    7;55 pm

--- scripts.js
+++ scripts.js
@@ -1,3 +1,41 @@
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.glider'), {
+    //To make responsive, check the documentation for break points
+  slidesToShow: 1,
+  slidesToScroll: 'auto',
+  // draggable: true,
+  // scrollLock: true,
+  dots: '#dots',
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  },
+  responsive: [
+      {
+      // screens greater than >= 775px
+      breakpoint: 931,
+      settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 2,
+          slidesToScroll: 'auto',
+          itemWidth: 300,
+          duration: 0.25
+      }
+      },{
+      // screens greater than >= 1024px
+      breakpoint: 1200,
+      settings: {
+          slidesToShow: 3,
+          slidesToScroll: 'auto',
+          itemWidth: 300,
+          duration: 0.25
+      }
+      }
+  ]
+});
+})
+
+
 particlesJS("particles-js", {
   "particles": {
     "number": {
